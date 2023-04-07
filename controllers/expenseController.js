4017f340d8393263668febb3f23b35d7348ec58c
Dimitri2105 <karanthakur577@gmail.com>
2023-04-07 10:25:56 +0530

    Making Expense App Production ready

--- controllers/expenseController.js
+++ controllers/expenseController.js
@@ -2,6 +2,7 @@ const Expense = require('../modals/expenseModal')
 const User = require('../modals/userModal')
 const path = require('path')
 const rootDir = require('../util/path')
+const sequelize = require('../database/database')
 const userAuthentication = require('../middleware/auth')
 
 exports.saveToStorage = async(req,res,next) =>{
@@ -12,34 +13,35 @@ exports.saveToStorage = async(req,res,next) =>{
 
     if (!amount || !description || !category) {
         return res.status(400).json({ error: 'Amount, description, and category fields are required' });
-        
       }
-      try {
+
+    const t = await sequelize.transaction();
+
+    try {
         const data = await Expense.create(
             {   expenseAmount:amount,
                 description:description, 
                 category:category,
-                userId:req.user.id
-
+                userId:req.user.id,
+            },
+            {
+                transaction:t
             });
-        console.log(data.expenseAmount)
         const totalExpense = Number(req.user.totalExpenses) + Number(data.expenseAmount)
-        console.log(totalExpense)
-        await User.update( {totalExpenses:totalExpense} , {where:{id:req.user.id}},)
-        .then(() =>{
-            res.status(201).json({ newExpense: data ,totalExpense : totalExpense})
-        })
-        .catch(error =>{
-            console.log(error)
-            res.status(500).json({error:error})
-        })
-        // res.status(201).json({ newExpense: data })
+
+        await User.update( {totalExpenses:totalExpense} , {where:{id:req.user.id},transaction:t})
+        
+        await t.commit()
+        
+        res.status(201).json({ newExpense: data ,totalExpense : totalExpense})
       
-      } catch (error) {
+    } catch (error) {
+        await t.rollback()
         console.log(error)
         res.status(500).json({ error: error });
-      }
+    }
 }
+
 
 exports.getAllUsers = async(req,res,next) =>{
     try{
@@ -57,18 +59,27 @@ exports.getAllUsers = async(req,res,next) =>{
 
 exports.deleteExpense = async(req,res,next) =>{
     console.log(req.params.id)
+    
     if(!req.params.id){
         res.status(400).json({err:"Missing ExpenseID"})
     }
-    try{
-        console.log("User id is >>>>>>>",req.user.id)
-        const expenseId = req.params.id
-        Expense.destroy({where:{id:expenseId,userId:req.user.id}})
-        console.log("expense Successfully destroyed")
 
-    }catch{
-        console.log(err)
-        req.status(500).json({error:error})
+    const t = await sequelize.transaction();
+    try{
+        const expenseId = req.params.id
+        const expense = await Expense.findOne({where:{id:expenseId,userId:req.user.id},transaction:t})
+        const totalExpense = Number(req.user.totalExpenses) - Number(expense.expenseAmount)
+
+        await expense.destroy({transaction:t})
+        await User.update( {totalExpenses:totalExpense} , {where:{id:req.user.id},transaction:t},)
+        
+        await t.commit()
+        res.status(201).json({success:true, totalExpense : totalExpense})
+
+    }catch(error){
+        await t.rollback()
+        console.log(error)
+        res.status(500).json({error:error})
 
     }
 }
